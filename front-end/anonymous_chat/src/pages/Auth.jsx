efb3b990fbd448c2b3eff10e7272b1bccb7abe2f
M Sai Kiran <116418856+MSaiKiran9@users.noreply.github.com>
2023-12-06 23:43:11 +0530

    chakra in && auth page bare bones added -> todo

---
+++ front-end/anonymous_chat/src/pages/Auth.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignIn=()=>{
+<h1>SignIn</h1>
+}
+const SignUp=()=>{
+<h1>Signup</h1>
+}
+const Auth = () => {
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth
