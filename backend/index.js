5ca20f84ec780209fff7679d7ec3432f44bf2873
M Sai Kiran <116418856+MSaiKiran9@users.noreply.github.com>
2024-02-20 21:12:27 +0530

    socketio server init

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
-console.log("hi");
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST'],
+      credentials: true
+    }
+  });
+  
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
